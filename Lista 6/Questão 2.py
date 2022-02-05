7ce8b1c0b810f37f7e5829be917cb3feb36a6935
Rener Crisostomo <rener.bluernr@gmail.com>
2022-02-04 22:49:03 -0300

    Update Questão 2.py

--- Lista 6/Questão 2.py
+++ Lista 6/Questão 2.py
@@ -3,7 +3,7 @@ QUESTÃO 2 LISTA 6
 Deseja-se fazer a emissão da folha de pagamento de uma empresa. Para os 4 funcionários de uma empresa são dadas as seguintes informações: (matrícula, nome, função e salário). Implemente uma matriz 4 x 4 para emissão dessas informações, depois mostre quem ganha o maior e o menor salário.
 """
 matriz, salarios = [[], [], [], []], []
-print("Digite as informações nessa formatação: matrícula, nome, função, salário")
+print("Digite as informações no formato: matrícula, nome, função, salário")
 for funcionario in range(len(matriz)):
     matriz[funcionario] = ((input(f"Funcionario {funcionario + 1}: ")).replace(" ","")).split(",")
     matriz[funcionario][3] = int(matriz[funcionario][3])
