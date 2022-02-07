81280c257f18fbb65605049e09a735ffaf658336
Rener Crisostomo <rener.bluernr@gmail.com>
2022-02-07 19:58:03 -0300

    update lista 7

--- Lista 7/Questão 3.py
+++ Lista 7/Questão 3.py
@@ -3,17 +3,8 @@ QUESTAO 3 LISTA 7
 Crie uma função em python que receba 3 números e retorne em forma crescente.
 """
 def cresente(num1, num2, num3):
-    numeros = [num1, num2, num3]
-    for num in range(3):
-        for linha in range(num):
-            if numeros[num] != numeros[linha]:
-                if numeros[num] > numeros[linha]:
-                    numeros.insert(linha, numeros[num]) 
-                else:
-                    numeros.insert(linha + 1, numeros[num])
-                numeros.pop(num + 1)
-                break
-    return numeros
+    numeros = [int(num1), int(num2), int(num3)]
+    return sorted(numeros)
 
 num1 = input("numero1 = ")
 num2 = input("numero2 = ")
--- Lista 7/Questão 5.py
+++ Lista 7/Questão 5.py
@@ -10,3 +10,21 @@ ENTRE 25,0 E 29,9   | SOBREPESO       | I
 ENTRE 30,0 E 39,9   | OBESIDADE       | II
 MAIOR QUE 40,0      | OBESIDADE GRAVE | III
 """
+
+def classificacaoCorporal (peso, altura):
+    print(f"\nIMC, CLASSIFICAÇÂO, OBESIDADE (GRAU)")
+    imc = peso / altura ** 2
+    if imc >= 40:
+        print(f"{round(imc, 2)}, OBESIDADE GRAVE, III")
+    elif imc >= 30:
+        print(f"{round(imc, 2)}, OBESIDADE, II")
+    elif imc >= 25:
+        print(f"{round(imc, 2)}, SOBREPESO, I")
+    elif imc >= 18.5:
+        print(f"{round(imc, 2)}, NORMAL, 0")
+    else:
+        print(f"{round(imc, 2)}, MAGREZA, 0")
+
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite seu altura (m): "))
+classificacaoCorporal (peso, altura)
